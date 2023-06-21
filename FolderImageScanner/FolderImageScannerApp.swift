833ecb9c39638e38eb642601c97786453b2a7d40
Taha Tuna <tahatuna777@gmail.com>
2023-06-21 06:50:40 +0200

    Initial Commit

---
+++ FolderImageScanner/FolderImageScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderImageScannerApp.swift
+//  FolderImageScanner
+//
+//  Created by Taha Tuna on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FolderImageScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
